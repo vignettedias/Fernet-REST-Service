abc14ad618060fa76227489458f074fbd21aede0
Vighnesh Das <dasvighnesh@gmail.com>
2026-06-05 11:33:06 +0530

    Complete Phase 6: Docker Compose and production hardening

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const app = require("./app");
 const { PORT } = require("./config/env");
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `Fernet REST Service running on port ${PORT}`
   );
+});
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM received");
+
+  server.close(() => {
+    console.log("HTTP server closed");
+    process.exit(0);
+  });
+});
+
+process.on("SIGINT", () => {
+  console.log("SIGINT received");
+
+  server.close(() => {
+    console.log("HTTP server closed");
+    process.exit(0);
+  });
 });
